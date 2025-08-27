6c1f2111d139705ec5578a76642d0dd522a9ee09
abhaychhani <abhaychhani@gmail.com>
2025-08-27 13:27:23 +0530

    main file created

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import * 
+
+root = Tk()
+root.title("Todo App Made with ❤️ By Code.Ide")
+root.config(bg="black")
+root.geometry("400x500")
+root.iconbitmap("./img/icon.ico")
+
+
+
+root.mainloop()
+
+
